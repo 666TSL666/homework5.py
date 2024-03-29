00f1cf3b2893bf6764189de4d6d4eed9812c7194
666TSL666 <163123757+666TSL666@users.noreply.github.com>
2024-03-29 14:58:15 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,19 @@
+#my_list = ['banana', 'coconut', 'pineapple', 'pear', 'grape']
+#print(my_list)
+#print(my_list [0], my_list[-1])
+#print(my_list [0])
+#print(my_list[-1])
+#print(my_list[2:5])
+#my_list[2] = 'melon'
+#print(my_list)
+
+my_dict = {'banana': "банан", 'coconut': "кокос", 'pineapple': "ананас", 'pear': "груша", 'grape': "виноград"}
+print(my_dict)
+print(my_dict.get('banana'))
+print(my_dict.get('coconut'))
+print(my_dict.get('pineapple'))
+print(my_dict.get('pear'))
+print(my_dict.get('grape'))
+my_dict.update({'banana': "cucumber", 'coconut': "cabbage", 'pineapple': "tomato", 'pear': "onion", 'grape': "garlic"})
+my_dict['kiwi'] = 'киви'
+print(my_dict)
